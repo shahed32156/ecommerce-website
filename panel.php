5da4768cdb53aa8cf1152391077089955ceedde3
Abdullah Shahed <142104079+shahed32156@users.noreply.github.com>
2024-04-23 14:37:48 +0600

    Add files via upload

---
+++ panel.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Panel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="panel.css">
+
+</head>
+
+<body>
+
+
+
+     <h1 style="text-align: center;">E-Commerce Website</h1>
+
+    <div class="container">
+
+    <div class="row">
+
+
+        <div class="col-lg-5">
+
+            <img class="image" src="main_page.jpg" alt="" height="350px" width="500px">
+        
+        </div>
+
+        <div class="col-lg-7 panel">
+
+            <div class="heading">
+                <h2 style="text-align: center;"> Choose your option </h2>
+                </div>
+        
+                <div class="admin" style="margin-top: 10%;">
+                <a href="admin_login.php" style="margin-left: 38%;">Admin Panel </a>
+                </div>
+        
+                <div class="student" style="margin-top: 35px;">
+                <a href="login.php" style="margin-left: 40%;">User Panel </a>
+                </div>
+
+
+        </div>
+
+
+
+    </div>
+
+
+    </div>
+
+    
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+
+</body>
+
+
+
+
+</html>
